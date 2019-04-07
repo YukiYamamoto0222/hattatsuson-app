4098488931098cdeb0b9de9c72a4f99c6de4d81a
Yuki Yamamoto <shinki.y.yamamoto@gmail.com>
2019-04-07 18:43:53 +0900

    data_create更新

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -37,7 +37,11 @@ class HealthController < ApplicationController
   end
 
   def data_create
-    @health = Health.last
+    if Health.find_by(user_id: params[:user_id], date: Date.today) == nil
+      @health = Health.new
+    else
+      @health = Health.last
+    end
     @health.rain_pct = params[:rain_pct]
     @health.w_temp = params[:w_temp]
     @health.y_steps = params[:y_steps]
