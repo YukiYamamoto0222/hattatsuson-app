1e13cb7b30bf9e33086a3d4dffecf83f91ec6666
Yuki Yamamoto <shinki.y.yamamoto@gmail.com>
2019-04-07 20:18:38 +0900

    user nameをカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_084828) do
+ActiveRecord::Schema.define(version: 2019_04_07_111714) do
 
   create_table "healths", force: :cascade do |t|
     t.integer "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_04_07_084828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "pref_code"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
